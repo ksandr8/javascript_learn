57bc6a12cdbfa56d7852bb7bcca55106abe80034
ksandr8 <tronvers@gmail.com>
2023-01-02 13:30:54 +0200

    Update 6. First app

---
+++ first app/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const numberOfFilms = +prompt('Скільки фільмів ви уже перегляунли?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один і останніх переглянутих фільмів?', ''),
+      b = prompt('Оцініть фільм?', ''),
+      c = prompt('Один і останніх переглянутих фільмів?', ''),
+      d = prompt('Оцініть фільм?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
--- js/start_learn.js
+++ js/start_learn.js
@@ -1,80 +1,81 @@
-"use strict"; // використання нового режиму
+// "use strict"; // використання нового режиму
 
-let number = 5;
-const leftBorderWidth = 1;
+// let number = 5;
+// const leftBorderWidth = 1;
 
-number = 10;
-console.log(number);
+// number = 10;
+// console.log(number);
 
-const obj = {
-    a: 50
-}; 
-obj.a = 10;
-console.log(obj.a);
+// const obj = {
+//     a: 50
+// }; 
+// obj.a = 10;
+// console.log(obj.a);
 
-var name = 'Alex';
-name = 'Ivan';
-console.log(name);
+// var name = 'Alex';
+// name = 'Ivan';
+// console.log(name);
 
-{
-    let num = 2; // не знайде змінної
-    var nums = 5; // виведе змінну
-}
-console.log(nums);
+// {
+//     let num = 2; // не знайде змінної
+//     var nums = 5; // виведе змінну
+// }
+// console.log(nums);
 
-const vehicleBodyWidth = 5000;
-const vehicleBodyLength = 4000;
-console.log ('Ширина авто: ' + vehicleBodyWidth + ', Висота авто: ' + vehicleBodyLength);
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+// console.log ('Ширина авто: ' + vehicleBodyWidth + ', Висота авто: ' + vehicleBodyLength);
 
-//array
-const array = ['a', 'b', 'c'];
-console.log(array[1]);
-
-
-//object
-const objct = {
-    'Anna': 500,
-    Girl: 450,
-    3:350
-};
-console.log(objct.Anna);
-console.log(objct.Girl);
-console.log(objct[3]);
-//add new properties to object
-objct.first = 455; // метод додавання нової властивості в об'єкт
-objct['second'] = 478; // другий метод додавання нової властивості в об'єкт
-console.log(objct.first); // метод вивода строки
-console.log(objct['first']); // другий метод вивода строки
+// //array
+// const array = ['a', 'b', 'c'];
+// console.log(array[1]);
 
 
-// Спілкування з користувачем
+// //object
+// const objct = {
+//     'Anna': 500,
+//     Girl: 450,
+//     3:350
+// };
+// console.log(objct.Anna);
+// console.log(objct.Girl);
+// console.log(objct[3]);
+// //add new properties to object
+// objct.first = 455; // метод додавання нової властивості в об'єкт
+// objct['second'] = 478; // другий метод додавання нової властивості в об'єкт
+// console.log(objct.first); // метод вивода строки
+// console.log(objct['first']); // другий метод вивода строки
 
-// alert("Hello"); //модальне вікно
-// const result = confirm("Are yuo here?"); //модальне вікно
-// console.log(result); 
-// const answer = prompt("Вам є 18?", ""); //модальне вікно з відповіддю текстом
-// console.log(answer); 
 
-// const answers = [];
-// answers[0]=prompt("Ваше ім'я","");
-// answers[1]=+prompt("Ваш вік","");
-// answers[2]=prompt("Ваша стать","");
-// console.log(answers);
-// document.write(answers);
+// // Спілкування з користувачем
 
-//Інтерполяція
+// // alert("Hello"); //модальне вікно
+// // const result = confirm("Are yuo here?"); //модальне вікно
+// // console.log(result); 
+// // const answer = prompt("Вам є 18?", ""); //модальне вікно з відповіддю текстом
+// // console.log(answer); 
 
-const category = 'toys';
-console.log(`https://someurl.com/${category}`);
+// // const answers = [];
+// // answers[0]=prompt("Ваше ім'я","");
+// // answers[1]=+prompt("Ваш вік","");
+// // answers[2]=prompt("Ваша стать","");
+// // console.log(answers);
+// // document.write(answers);
 
-//Оператори в JS
+// //Інтерполяція
 
-let incr = 10,
-    decr = 10;
-incr++; // інкремент добавляє 1
-decr--; // декремент мінусує 1
-console.log(incr);
-console.log(decr);
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}`);
 
+// //Оператори в JS
+
+// let incr = 10,
+//     decr = 10;
+// incr++; // інкремент добавляє 1
+// decr--; // декремент мінусує 1
+// console.log(incr);
+// console.log(decr);
+
+// console.log(decr);
 
 
